cd9febeba6330c6cbf05601ff6cf649b27659b49
John Tran <jtran6796@gmail.com>
2026-05-07 03:42:05 -0400

    changed backend_url for deployment

--- extension/lib/config.js
+++ extension/lib/config.js
@@ -1,4 +1,5 @@
 // Update this URL after deploying your worker.
 // For local dev: http://127.0.0.1:8787
 // For production: https://learnly-backend.<your-subdomain>.workers.dev
-export const BACKEND_URL = "http://127.0.0.1:8787";
+// export const BACKEND_URL = "http://127.0.0.1:8787";
+export const BACKEND_URL = "https://learnly-backend.learnly.workers.dev";
